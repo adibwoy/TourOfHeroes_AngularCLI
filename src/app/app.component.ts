092b4bd454b2410e232103a3014f00e9b088b8a3
Aditya Bhandari <adityab@vmware.com>
2016-12-02 06:25:13 -0800

    [INTERNAL] Changed project indent size to 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,27 +4,27 @@ import { Hero } from './hero-detail/hero';
 import { HeroService } from './hero.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [HeroService]
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    providers: [HeroService]
 })
 export class AppComponent implements OnInit {
-  title = 'Tour of Heroes';
-  heroes: Hero[];
-  selectedHero: Hero;
+    title = 'Tour of Heroes';
+    heroes: Hero[];
+    selectedHero: Hero;
 
-  onSelect(hero:Hero) {
-    this.selectedHero = hero;
-  }
+    onSelect(hero: Hero) {
+        this.selectedHero = hero;
+    }
 
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
+    getHeroes(): void {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
 
-  ngOnInit(): void {
-    this.getHeroes();
-  }
+    ngOnInit(): void {
+        this.getHeroes();
+    }
 
-  constructor(private heroService: HeroService) {}
+    constructor(private heroService: HeroService) { }
 }
--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -4,13 +4,13 @@ import { TestBed, async, inject } from '@angular/core/testing';
 import { HeroService } from './hero.service';
 
 describe('HeroService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [HeroService]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [HeroService]
+        });
     });
-  });
 
-  it('should ...', inject([HeroService], (service: HeroService) => {
-    expect(service).toBeTruthy();
-  }));
+    it('should ...', inject([HeroService], (service: HeroService) => {
+        expect(service).toBeTruthy();
+    }));
 });
