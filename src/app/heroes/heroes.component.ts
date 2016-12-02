e2675d2b3fdc97e2e45c21277bdd43f9fdbe4c66
Aditya Bhandari <adityab@vmware.com>
2016-12-02 07:49:45 -0800

    Routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero-detail/hero';
+import { Router } from '@angular/router';
 
+import { Hero } from '../hero-detail/hero';
 import { HeroService } from '../hero.service';
 
 @Component({
@@ -12,9 +13,9 @@ export class HeroesComponent implements OnInit {
     heroes: Hero[];
     selectedHero: Hero;
 
-    onSelect(hero: Hero) {
-        this.selectedHero = hero;
-    }
+    constructor(
+        private router: Router,
+        private heroService: HeroService) { }
 
     getHeroes(): void {
         this.heroService.getHeroes().then(heroes => this.heroes = heroes);
@@ -24,6 +25,11 @@ export class HeroesComponent implements OnInit {
         this.getHeroes();
     }
 
-    constructor(private heroService: HeroService) { }
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
 
+    gotoDetail(): void {
+        this.router.navigate(['/detail', this.selectedHero.id]);
+    }
 }
